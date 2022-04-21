8bf41e6d1b73e50eda5c79534c0e094d1ff415f8
catcoding-code <ca.dawson123@gmail.com>
2022-04-21 13:04:57 +1000

    Add content

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import './App.css';
+import { useState } from 'react'
+
+const getImages= async query => {
+  const url= "https://unsplash-api.dawso-demo.workers.dev/"
+
+  const resp = await fetch(url, {
+    method: "POST",
+    body: JSON.stringify({ query }),
+    headers: {'Content-type': 'application/json'}
+  })
+
+  return resp.json()
+}
+
+function App() {
+  const [query, setQuery]= useState("")
+
+  const updateQuery = evt => setQuery(evt.target.value)
+  const [images, setImages] = useState([])
+
+  const search= async () => {
+    const results = await getImages(query)
+    setImages(results)
+  }
+  
+  return (
+    <div className="App">
+      <div className="form">
+        <input id="query" type="text" onChange={updateQuery} placeholder="Search query" />
+        <button onClick={search}>Search</button>
+      </div>
+
+      {images.map(image =>
+        <a key={image.id} href={image.link} target="_blank">
+          <img src={image.image} />  
+        </a>
+      )}
+    </div>
+  );
+}
+
+export default App;
